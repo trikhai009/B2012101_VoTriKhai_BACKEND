9232c66d989128268829da6da371cb9166363c7e
trikhai009 <vokhai.271002@gmail.com>
2023-02-21 08:36:14 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");///
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");////
 
 const app = express();
 
@@ -10,5 +11,19 @@ app.get("/", (req, res)=>{
     res.json({message:"Welcom to contact book application."});
 });
 app.use("/api/contacts", contactsRouter);
+
+//handler 404 res
+app.use((req, res, next) => {
+    //for all route
+    //matched -> goi next de chuyen sang midlwer xu li loi
+    return next(new ApiError(404, "Resource not found"));
+});
+
+//define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
